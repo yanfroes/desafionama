d41890872803d30a4671a18ed070ab1beec6ecbc
Yan Froes <yanfroes@gmail.com>
2021-03-20 19:43:28 -0300

    first challenge completed

--- lib/nama_challenge.rb
+++ lib/nama_challenge.rb
@@ -1,3 +1,20 @@
-class NamaChallenge
+i = 1
+max = 100
 
+while i <= max do
+  if i % 100 == 0
+    print " Nama"
+  elsif i % 35 == 0
+    print " Nama Team,"
+  elsif i % 5 == 0
+    print " Nama,"
+  elsif i % 7 == 0
+    print " Team,"
+  else
+    print " #{i},"
+  end
+  i = i+1
 end
+
+# print 2%1==0? "sim":"não"
+# poderia fazer utilizando essa lógica no print também
